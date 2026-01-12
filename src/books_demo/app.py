abea724164bc03ac23ba5192df3bfdb5108e9337
Natalia Dudzińska <01187218@pw.edu.pl>
2026-01-12 11:45:58 +0100

    refactor: refactor locale manager

--- src/books_demo/app.py
+++ src/books_demo/app.py
@@ -3,11 +3,25 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from sqlmodel import Session, select
 
+from modeltranslation.locale_manager import apply_translation
+from modeltranslation.translator import TranslationOptions, Translator
+
 from .database import create_db_and_tables, create_db_engine
 from .models import Book
 from .seed_data import seed_data
 
+translator = Translator(
+    default_language="en",
+    languages=("en", "pl"),
+)
+
 engine = create_db_engine(in_memory=False)
+
+
+@translator.register(Book)
+class BookTranslationOptions(TranslationOptions):
+    fields = ("title",)
+    required_languages = ("en",)
 
 
 @asynccontextmanager
@@ -19,8 +33,17 @@ async def lifespan(_: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+apply_translation(app, translator)
+
 
 @app.get("/books/")
 def get_books() -> list[Book]:
     with Session(engine) as session:
         return session.exec(select(Book)).all()
+
+
+@app.get("/books/titles", response_model=list[str])
+def get_books_titles() -> list[Book]:
+    with Session(engine) as session:
+        books = session.exec(select(Book)).all()
+        return [book.title for book in books]
